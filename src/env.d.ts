b64d3b41f1dd304a31b1281ad83ad98c323328ab
kiyo-e <kiyo-e@users.noreply.github.com>
2026-06-19 21:50:54 +0900

    Simplify Workers config and docs

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,8 +2,6 @@ declare global {
   interface Env extends CloudflareBindings {
     CLOUDFLARE_ACCOUNT_ID: string;
     CLOUDFLARE_API_TOKEN: string;
-    R2_ACCESS_KEY_ID: string;
-    R2_SECRET_ACCESS_KEY: string;
 
     GITHUB_TOKEN?: string;
     GITHUB_WEBHOOK_SECRET: string;
